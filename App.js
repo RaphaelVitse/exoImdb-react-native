60000408f9554cccc9b6deae9392e7a3a6b2cb64
Raphael Vitse <vitseraphael@gmail.com>
2024-12-02 15:37:37 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  SafeAreaView,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+} from "react-native";
+
+import Constants from "expo-constants";
+import logoImdb from "./assets/img/logoImdb.png";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Image
+          source={require("./assets/img/logoImdb.png")}
+          style={{ width: 100, height: 100 }}
+        />
+        <Text>hello !</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Platform.OS === "android" ? Constants.statusBarHeight : 0,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flexStart",
   },
 });
